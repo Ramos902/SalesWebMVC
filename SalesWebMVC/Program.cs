437a754266d9a4a6ee2c337f8d06b39776f289d8
João Ramos <joaopedroramos1512@gmail.com>
2025-08-13 21:31:01 -0300

    Connecting app to Database

--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -1,9 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Pomelo.EntityFrameworkCore.MySql.Internal;
 using SalesWebMVC.Data;
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<SalesWebMVCContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWebMVCContext") ?? throw new InvalidOperationException("Connection string 'SalesWebMVCContext' not found.")));
+try
+{
+    builder.Services.AddDbContext<SalesWebMVCContext>(options =>
+        options.UseMySql(builder.Configuration.GetConnectionString("SalesWebMVCContext"),
+        new MySqlServerVersion(new Version(8, 0, 43)),
+        MySqlOptions => MySqlOptions.MigrationsAssembly("SalesWebMVC")));
+}
+catch (InvalidOperationException)
+{
+    throw new InvalidOperationException("Connection string 'SalesWebMVCContext' not found.");
+}
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
